b5239161c217d382943503bb7e1729d47bf661ce
Rishab Dhar <rishab.dhar@magnolia-cms.com>
2023-12-19 16:40:35 +0530

    gRPC API to notify about webook events

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go
 go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 export PATH="${PATH}:${HOME}/go/bin"
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative api/watchtower.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative api/git-webhook-grpc.proto
