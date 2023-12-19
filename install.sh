7ff7af15bfb839dd32a116b2ce647e983843d1eb
Rishab Dhar <rishab.dhar@magnolia-cms.com>
2023-12-19 15:36:09 +0530

    Watchtower grpc API

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+go install google.golang.org/protobuf/cmd/protoc-gen-go
+
+go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+export PATH="${PATH}:${HOME}/go/bin"
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative api/watchtower.proto
